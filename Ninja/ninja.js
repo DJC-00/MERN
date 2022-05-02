6af59d00b937effd105abdf69bdd1c766aab6893
DJC-00 <djcastle2172@gmail.com>
2022-05-02 16:02:39 -0500

    Super Ninja

--- Ninja/ninja.js
+++ Ninja/ninja.js
@@ -25,7 +25,29 @@ class Ninja {
 
 }
 
-let Bro = new Ninja("DJ");
-Bro.sayName();
-console.log(Bro.getStats())
-console.log(Bro.drinkSake())
+class Sensei extends Ninja {
+    constructor(name, health = 100, speed = 3, strength = 3, wisdom = 10) {
+        super(name, health, speed, strength)
+        this.wisdom = wisdom;
+    }
+
+    getStats() {
+        const { speed, strength, wisdom } = this
+        return { speed, strength, wisdom };
+    }
+
+    speakWisdom() {
+        return "What one programmer can do in one month, two programmers can do in two months."
+    }
+}
+
+let newNinja = new Ninja("DJ");
+newNinja.sayName();
+console.log(newNinja.getStats())
+console.log(newNinja.drinkSake())
+
+let newSensei = new Sensei("Shredder", undefined, 5)
+newSensei.sayName();
+console.log(newSensei.getStats())
+console.log(newSensei.drinkSake())
+console.log(newSensei.speakWisdom());
