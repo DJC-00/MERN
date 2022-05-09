a77ec6f35cb43fe82907c9fb77ad47841927683a
DJC-00 <djcastle2172@gmail.com>
2022-05-09 18:50:03 -0500

    nope

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,22 +1,37 @@
-import React, { useState } from 'react';
-import MessageForm from './components/MessageForm';
-import MessageDisplay from './components/MessageDisplay';
+import React from "react";
+import { useParams } from "react-router";
+import {
+  BrowserRouter,
+  Link,
+  Switch,
+  Route
+} from "react-router-dom";
     
+const Location = (props) => {
+  const { city } = useParams();
+    
+  return (
+    <h1>Welcome to { city }!</h1>
+  );
+}
     
 function App() {
-    const [currentMsg, setCurrentMsg] = useState("There are no messages");
-    
-    const youveGotMail = ( newMessage ) => {
-        setCurrentMsg( newMessage );
-    }
-    return (
-        <>
-        <div style={{padding:'20px'}}>
-            <MessageForm onNewMessage={ youveGotMail } />
-            <MessageDisplay message={ currentMsg } />
-        </div>
-        </>
-    );
+  return (
+    <BrowserRouter>
+      <p>
+        <Link to="/location/seattle">Seattle</Link>
+        &nbsp;|&nbsp;
+        <Link to="/location/chicago">Chicago</Link>
+        &nbsp;|&nbsp;
+        <Link to="/location/burbank">Burbank</Link>
+      </p>
+      <Switch>
+        <Route path="/location/:city">
+          <Location />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
     
 export default App;
---
+++ test-app/src/components/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+    
+const About = (props) => {
+    
+    return (
+        <h1 style={{color:'blue'}}>About</h1>
+    );
+};
+    
+export default About;
